1b7c90202773e811d6858d18bce185464ba41fcc
yungsamd17 <yungsamd@proton.me>
2023-10-20 23:12:33 +0200

    🔧Update (HideNitroUpsell Plugin): Message hover super reaction buttons
    - Super Reaction message hover button
    - Super Reaction tab in menu 'picker'
    - Update comments on what hides what

--- Plugins/HideNitroUpsell/HideNitroUpsell.plugin.js
+++ Plugins/HideNitroUpsell/HideNitroUpsell.plugin.js
@@ -2,7 +2,7 @@
  * @name Hide Nitro Upsell
  * @description Hides all upsell of Nitro in common used UI across the Discord.
  * @author Sam @yungsamd17
- * @version 1.0
+ * @version 1.1
  * @authorId 402083911053869056
  * @invite GYveRfruk6
  * @donate https://ko-fi.com/yungsamd17
@@ -16,29 +16,40 @@ module.exports = () => ({
 
   start() {
     function hideElements() {
-      // Hide all <a> elements with href="/store"
+      // Store button link in Direct Messages
       const storeLinks = document.querySelectorAll('a[href="/store"]');
       storeLinks.forEach(link => {
         link.style.display = 'none';
       });
 
-      // Hide the <div> with id="message-add-reaction-1"
+      // Super Reaction in message right click context menu
       const reactionDiv = document.getElementById('message-add-reaction-1');
       if (reactionDiv) {
         reactionDiv.style.display = 'none';
       }
 
-      // Hide the <button> with aria-label="Send a gift"
+      // Gift button in message input box
       const giftButton = document.querySelector('button[aria-label="Send a gift"]');
       if (giftButton) {
         giftButton.style.display = 'none';
       }
 
-      // Hide the <div> with class containing 'premiumFeatureBannerBackground'
+      // Profile decoration banner for Nitro Shop in Profile settings
       const premiumBannerDivs = document.querySelectorAll('[class*="premiumFeatureBannerBackground-"]');
       premiumBannerDivs.forEach(div => {
         div.style.display = 'none';
-      });
+      })
+
+      // Small "Add Super Reaction" button on right of the message when hovering over it
+      const smallReactionDiv = document.querySelectorAll('[aria-label="Add Super Reaction"]');
+      smallReactionDiv.forEach(div => {
+        div.style.display = 'none';
+      })
+      // Super Reaction tab in Reactions menu 'picker'
+      const superReactionTab = document.getElementById('super-reaction-picker-tab');
+      if (superReactionTab) {
+        superReactionTab.style.display = 'none';
+      };
     }
 
     // Function to observe DOM mutations and reapply the hiding logic
